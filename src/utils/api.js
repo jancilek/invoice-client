bbe3a31111ecdb3ca662f72618b5483f8e5fdc8a
Jan Cilek <jan.cilek@gmail.com>
2024-05-20 16:27:14 +0200

    Updated backend url for fetch

--- src/utils/api.js
+++ src/utils/api.js
@@ -20,63 +20,63 @@
  * Více informací na http://www.itnetwork.cz/licence
  */
 
-
-const API_URL = "http://localhost:8080";
+const API_URL = "https://invoice-server-yefc.onrender.com";
 
 const fetchData = (url, requestOptions) => {
-    const apiUrl = `${API_URL}${url}`;
+  const apiUrl = `${API_URL}${url}`;
 
-    return fetch(apiUrl, requestOptions)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`Network response was not ok: ${response.status} ${response.statusText}`);
-            }
+  return fetch(apiUrl, requestOptions)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(
+          `Network response was not ok: ${response.status} ${response.statusText}`
+        );
+      }
 
-            if (requestOptions.method !== 'DELETE')
-                return response.json();
-        })
-        .catch((error) => {
-            throw error;
-        });
+      if (requestOptions.method !== "DELETE") return response.json();
+    })
+    .catch((error) => {
+      throw error;
+    });
 };
 
 export const apiGet = (url, params) => {
-    const filteredParams = Object.fromEntries(
-        Object.entries(params || {}).filter(([_, value]) => value != null)
-    );
+  const filteredParams = Object.fromEntries(
+    Object.entries(params || {}).filter(([_, value]) => value != null)
+  );
 
-    const apiUrl = `${url}?${new URLSearchParams(filteredParams)}`;
-    const requestOptions = {
-        method: "GET",
-    };
+  const apiUrl = `${url}?${new URLSearchParams(filteredParams)}`;
+  const requestOptions = {
+    method: "GET",
+  };
 
-    return fetchData(apiUrl, requestOptions);
+  return fetchData(apiUrl, requestOptions);
 };
 
 export const apiPost = (url, data) => {
-    const requestOptions = {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(data),
-    };
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  };
 
-    return fetchData(url, requestOptions);
+  return fetchData(url, requestOptions);
 };
 
 export const apiPut = (url, data) => {
-    const requestOptions = {
-        method: "PUT",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(data),
-    };
+  const requestOptions = {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  };
 
-    return fetchData(url, requestOptions);
+  return fetchData(url, requestOptions);
 };
 
 export const apiDelete = (url) => {
-    const requestOptions = {
-        method: "DELETE",
-    };
+  const requestOptions = {
+    method: "DELETE",
+  };
 
-    return fetchData(url, requestOptions);
+  return fetchData(url, requestOptions);
 };
